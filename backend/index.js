71b0d0b8002367bb9ef31824e8f2ed87c7492356
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-06 14:38:55 +0200

    create errorHandler

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
+const error = require('./middleware/ErrorHandlingMiddleware')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 
@@ -11,7 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api',router)
-
+app.use(errorHandler)
 
 const start = async()=>{
     try{
