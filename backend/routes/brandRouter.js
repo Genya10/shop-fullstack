4d12368b2e00444faaf734315990bdbcfc184b82
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-07 10:11:13 +0200

    install fileupload

--- backend/routes/brandRouter.js
+++ backend/routes/brandRouter.js
@@ -2,6 +2,7 @@ const Router = require('express')
 const router = new Router()
 const BrandController = require('../controllers/brandController')
 
+
 router.post('/',BrandController.create)
 router.get('/', BrandController.getAll)
 
