edc2359547470ba9297d222b1ede7487a0fa8119
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-08 13:02:41 +0200

    add DeviceStore to Context

--- frontend/src/store/DeviceStore.js
+++ frontend/src/store/DeviceStore.js
@@ -7,22 +7,33 @@ export class DeviceStore {
         {id:2,name:'Smartphone'}
        ]
        this._brands=[
-        {id:1,name:'Sansung'},
+        {id:1,name:'Samsung'},
         {id:2,name:'Apple'}
        ]
+       this._devices =[
+        {id:1,name:"Smartphone",price:12000,img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomfy.ua%2Fsmartfon-xiaomi-redmi-note-12-8-256gb-sunrise-gold.html&psig=AOvVaw2RqqDyIpErOwyrNGbemPSC&ust=1699526947149000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCK-7SdtIIDFQAAAAAdAAAAABAo"},
+        {id:2,name:"Smartphone",price:12000,img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomfy.ua%2Fsmartfon-xiaomi-redmi-note-12-8-256gb-sunrise-gold.html&psig=AOvVaw2RqqDyIpErOwyrNGbemPSC&ust=1699526947149000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCK-7SdtIIDFQAAAAAdAAAAABAo"},
+        {id:3,name:"Smartphone",price:12000,img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomfy.ua%2Fsmartfon-xiaomi-redmi-note-12-8-256gb-sunrise-gold.html&psig=AOvVaw2RqqDyIpErOwyrNGbemPSC&ust=1699526947149000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCK-7SdtIIDFQAAAAAdAAAAABAo"},
+        {id:4,name:"Smartphone",price:12000,img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomfy.ua%2Fsmartfon-xiaomi-redmi-note-12-8-256gb-sunrise-gold.html&psig=AOvVaw2RqqDyIpErOwyrNGbemPSC&ust=1699526947149000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCK-7SdtIIDFQAAAAAdAAAAABAo"},
+    ]
         makeAutoObservable(this)
     }
-
-    setIsAuth(bool){
-        this._isAuth = bool
+    setTypes(types){
+        this._types = types
     }
-    setIsUser(user){
-        this._isAuth = user
+    setBrands(brands){
+        this._brands = brands
     }
-    get isAuth(){
-        return this._isAuth
+    setIsUser(devices){
+        this._devices = devices
     }
-    get user(){
-        return this._user
+    get types(){
+        return this._types
+    }
+    get brands(){
+        return this._brands
+    }
+    get devices(){
+        return this._devices
     }
 }
