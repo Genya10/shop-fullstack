1cc5ac8702772a9f19e609e9badf0b65a59c3610
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-09 20:35:41 +0200

    add jwt_decode in userAPI

--- frontend/src/http/userAPI.js
+++ frontend/src/http/userAPI.js
@@ -1,13 +1,14 @@
 import { $authHost,$host } from "./indexHttp";
+import * as jwtDecode from "jwt-decode";
 
 export const registration = async(email,password)=>{
-    const response = await $host.post('api/user/registration',{email,password,role:'ADMIN'})
-    return response
+    const {data} = await $host.post('api/user/registration',{email,password,role:'ADMIN'})
+    return jwtDecode(data.token)
 }
 
 export const login = async(email,password)=>{
-    const response = await $host.post('api/user/login',{email,password})
-    return response
+    const {data} = await $host.post('api/user/login',{email,password})
+    return jwtDecode(data.token)
 }
 
 export const check = async()=>{
