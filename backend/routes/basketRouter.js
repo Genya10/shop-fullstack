d4b0a32b79f553d8d1808f82b2e58606c2b0c08b
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-17 10:28:50 +0200

    create basketRouter.js

---
+++ backend/routes/basketRouter.js
@@ -0,0 +1,7 @@
+const Router = require('express')
+const router = new Router()
+const basketController = require('../controllers/basketController')
+const authMiddleware = require('../middleware/authMiddleWare')
+
+router.get('/',authMiddleware,basketController.getBasketUser)
+router.post('/',authMiddleware,basketController.addBasket)
